dd20ae03c87d8d305080365b8896486b5ad78675
E1_83818_Harsh <soniharsh026@gmail.com>
2024-03-07 13:04:38 -0500

    added Q9 prep assignment

---
+++ prepAssignQ9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number:");
+    scanf("%d", &n);
+
+    printf("Give nnumber equivaent %d\n", n);
+    printf("Octal equivaent %o\n", n);
+    printf("Hexadecima equivaent %x\n", n);
+    
+    return 0;
+} 
